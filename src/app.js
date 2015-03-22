64441b2e597b6b83604b75ebf287fbc22d395ef2
Devin Jin <jinyucong911@outlook.com>
2015-03-22 16:27:07 +0800

    add path for static resources

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ var run = function() {
     mongoose.connect('localhost:27017/test');
     app = express();
 
+    app.use('/static', express.static(__dirname + '/public'));
+
     app.route('/users').get(function(request, response) {
         user = require('./modules/user');
         user.getAll(response);
