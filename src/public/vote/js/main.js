2be04a8db3e731406ee10bd2404566127c305b22
Jin Yucong <jinyucong911@outlook.com>
2015-04-03 16:48:27 +0800

    refine the page

--- src/public/vote/js/main.js
+++ src/public/vote/js/main.js
@@ -2,13 +2,27 @@ require.config({
   paths: {
     "jquery": 'http://code.jquery.com/jquery-1.10.2.min',
     "jqueryMobile": 'http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min'
+  },
+  shim: {
+    'jqueryMobile': ['jquery', 'jquery.mobile-config']
   }
 });
 
-require(['jquery', 'jqueryMobile'], function($, Mobile){
+define('jquery.mobile-config', ['jquery'], function ($) {
+  $(document).on('mobileinit', function () {
+    $.mobile.autoInitializePage = false;
+  });
+});
+
+require(['jquery', 'jqueryMobile'], function($){
   $(document).on("pageinit", "#add-vote-page", function() {
     $("#add-vote-btn").on("click", function() {
-      alert("!!");
-    })
+      $("#vote-options").append('<li class="option"><a>option</span></a>').listview('refresh');
+
+      $(".option").on("click", function() {
+        alert("haha");
+      });
+    });
   });
+  $.mobile.initializePage();
 });
