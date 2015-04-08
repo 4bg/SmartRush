6b06a42fb6d75e9a1c4d18dd3ea15a83d2d54190
Jin Yucong <jinyucong911@outlook.com>
2015-04-08 12:06:37 +0800

    use cdn for the libraries

--- src/public/vote/js/main.js
+++ src/public/vote/js/main.js
@@ -1,7 +1,8 @@
 require.config({
   paths: {
-    'jquery': 'http://code.jquery.com/jquery-1.10.2.min',
-    'jqueryMobile': 'http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min',
+    'jquery': 'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min',
+    'jqueryMobile': 'http://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.5/jquery.mobile.min',
+    'text': 'http://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text'
   },
   shim: {
     'jqueryMobile': ['jquery', 'jquery.mobile-config']
