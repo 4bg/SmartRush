83ccf6739829419ed9217cce7845c64d47392c57
Jin Yucong <jinyucong911@outlook.com>
2015-04-08 17:31:11 +0800

    add router and body parser for vote apis

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 express = require('express');
 mongoose = require('mongoose');
 config = require('./config');
+bodyParser = require('body-parser');
 
-mongoose.connect(config.db);
+// mongoose.connect(config.db);
 app = express();
 
 //static resouces
 app.use('/static', express.static(__dirname + '/public'));
+
+//support json-encoded bodies
+app.use( bodyParser.json() ); 
 
 //to use router
 router = require('express-routeloader')({});
