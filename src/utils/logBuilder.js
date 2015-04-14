955ed00d9070da4f3c012d6285c68801b6198bcb
Jin Yucong <jinyucong911@outlook.com>
2015-04-14 17:16:36 +0800

    Add log in wechat enterprise util

--- src/utils/logBuilder.js
+++ src/utils/logBuilder.js
@@ -2,3 +2,28 @@ exports.getLogByError = function (err) {
   var log = err.stack + '\n*************************************************************************\n';
   return log;
 }
+
+exports.getLogByRequest = function (request) {
+  var log = '';
+  if (request.method) {
+    log += request.method + ' ';
+  }
+
+  if (request.url) {
+    log += request.url;
+  }
+
+  if (request.param) {
+    log += '\n[param]\t' + JSON.stringify(request.param);
+  }
+
+  if (request.response) {
+    log += '\n[response]\t' + JSON.stringify(request.response);
+  } else {
+    log += '\n[response]\t' + 'No response';
+  }
+
+  log += '\n**************************************************************************************************\n';
+
+  return log;
+}
--- src/utils/wechatEnterprise.js
+++ src/utils/wechatEnterprise.js
@@ -1,14 +1,37 @@
-config = require('../config');
-request = require('request');
+var config = require('../config');
+var request = require('request');
+var logBuilder = require('./logBuilder');
+var logger = require('./logger');
 
 function getToken(callback) {
-  request.get('https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid='
+  var url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid='
   + config.corpId + '&corpsecret='
-  + config.secret,
-  function(error, response, body) {
+  + config.secret;
+  request.get(url, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      token = (JSON.parse(body)).access_token;
-      callback(token);
+      response = (JSON.parse(body));
+      var log = logBuilder.getLogByRequest({
+        method: 'GET',
+        url: url,
+        param: {
+          corpid: config.corpid,
+          corpsecret: config.secret
+        },
+        response: response
+      });
+      if(response.access_token && response.access_token.length > 0) {
+        logger.info(log, 'wechat-api');
+        token = response.access_token;
+        callback(token);
+      } else if (response.errmsg) {
+        logger.error(log, 'wechat-api');
+        throw new Error('Failed to get the token, error message is: ' + response.errmsg);
+      } else {
+        logger.error(log, 'wechat-api')
+        throw new Error('Unexpected response from wechat api');
+      }
+    } else {
+      throw new Error('Error when querying the wechat api');
     }
   })
 }
@@ -26,7 +49,15 @@ exports.sendTextMessageToApp = function(content, agentId, safe) {
       safe: safe
     };
     request.post({url: url, body: JSON.stringify(params)}, function(error, response, body) {
-      console.log(body);
+      //log this request
+      var requestResponse = JSON.parse(body);
+      var log = logBuilder.getLogByRequest({
+        method: 'POST',
+        url: url,
+        param: params,
+        response: requestResponse
+      });
+      logger.info(log, 'wechat-api');
     })
   })
 };
@@ -35,11 +66,22 @@ exports.getUserByCode = function(code, agentId, success) {
   getToken(function(token) {
     var url = 'https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=' + token + '&code=' + code + '&agentid=' + agentId;
     request.get(url, function(error, response, body) {
+      var requestResponse = JSON.parse(body);
+      var log = logBuilder.getLogByRequest({
+        method: 'GET',
+        url: url,
+        param: {
+          code: code,
+          agentid: agentId
+        },
+        response: requestResponse
+      });
       if (!error && response.statusCode === 200) {
-        user = JSON.parse(body);
-        success(user);
+        logger.info(log, 'wechat-api');
+        success(requestResponse);
       } else {
-        console.log('error when get user by id!');
+        logger.error(log, 'wechat-api');
+        throw new Error('Failed to get the user info from Wechat');
       }
     })
   })
