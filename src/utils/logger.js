2ce47273e5d85e2379fb2df446e637da9fbf12d8
Jin Yucong <jinyucong911@outlook.com>
2015-04-14 17:16:15 +0800

    Fix bug for logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,13 +1,19 @@
 var log4js = require('log4js');
 var timeFormatter = require('./timeFormatter');
+var config = require('../config');
+
+//configuration for log4js
+log4js.configure({
+  appenders: [
+    {type: 'file', filename: '../logs/app' + ' ' + timeFormatter.getFormattedDate() + '.log', category: 'app'},
+    {type: 'file', filename: '../logs/wechat-api' + ' ' + timeFormatter.getFormattedDate() + '.log', category: 'wechat-api'}
+  ]
+});
 
 function getLogger(category) {
   if (!category) {
     category = 'app';
   }
-  log4js.loadAppender('file');
-  var logFile = '../logs/' + category + ' ' + timeFormatter.getFormattedDate() + '.log';
-  log4js.addAppender(log4js.appenders.file(logFile), category);
   var logger = log4js.getLogger(category);
   return logger;
 }
